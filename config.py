7528a768b379374d98caf203a717bb24ec042687
metallicat20 <alex.clipper@gmail.com>
2018-07-01 21:17:57 +1000

    sample config

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "xxxxxxxxxxxxxxxxx"
+BOT_ADDRESS = "t.me/botname"
+URL = "http://127.0.0.1:35653/gui"
+USER = "utorrent_user"
+PASSWORD = "utorrent_password"
+ALLOWED_USERS = [11111111,2222222]
+KODI = { 'ip': "192.168.250.25:8080",
+         "username": "xbmc",
+         "password": "xbmc" }
